a069320e25ee98ef8520aa7f1e7f256587c8e345
aliesi3410 <52535627+aliesi3410@users.noreply.github.com>
2023-10-19 18:32:51 +0330

    v6

--- js/main.js
+++ js/main.js
@@ -22,29 +22,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   function scrollSpy() {
-    // var headerMenu;
+    // declaring body sections (containers)
     const [
       homeContainer,
       aboutContainer,
       portfolioContainer,
       serviceContainer,
+      testimonialsContainer,
+      blogContainer,
+      contactContainer
     ] = [
       document.getElementById("homeContainer"),
       document.getElementById("aboutContainer"),
       document.getElementById("portfolioContainer"),
       document.getElementById("serviceContainer"),
+      document.getElementById("testimonialsContainer"),
+      document.getElementById("blogContainer"),
+      document.getElementById("contactContainer")
     ];
     const containers = [
       homeContainer,
       aboutContainer,
       portfolioContainer,
       serviceContainer,
+      testimonialsContainer,
+      blogContainer,
+      contactContainer
     ];
     for (i in containers) {
       scrollSituation(containers[i], ++i);
       i++;
-      // headerMenu = document.querySelector(`#headerBtns :nth-child(${i})`);
-      // console.log(headerMenu);
     }
   }
   // change buttons classlist after certain pageContainers distance from top of the window is less than 0
@@ -52,14 +59,62 @@ document.addEventListener("DOMContentLoaded", () => {
     var pageSituation = pageContainer.getBoundingClientRect().top;
     var headerBtn = document.querySelector(`#headerBtns :nth-child(${order})`);
     var menuBtns = document.querySelectorAll(".header-menu-item");
-    if (pageSituation <= 20) {
+    if (pageSituation <= 70) {
       menuBtns.forEach((menuBtn) => {
         menuBtn.classList.remove("active");
       });
       headerBtn.classList.add("active");
-    } else {
-      headerBtn.classList.remove("active");
     }
   }
   fixHeader();
+});
+
+// swiper js:
+
+var swiper = new Swiper(".testimonialSlider", {
+  slidesPerView: 1,
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+  },
+  spaceBetween: 24,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// blog Slider
+var swiper = new Swiper(".blogSwiper", {
+  slidesPerView: 2,
+  breakpoints: {
+    1200:{
+      slidesPerView: 6,
+    },
+    992:{
+      slidesPerView: 4, 
+    },
+    576:{
+      slidesPerView: 3
+    },
+   },
+  spaceBetween: 24,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
 });
